cf40d7d648252f381a0b975b0fd925a10058bffb
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-11 12:52:08 -0300

    "feat: Adiciona validação lombok"

--- src/main/java/br/com/ruangomes/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/ruangomes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.ruangomes.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,11 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços em branco.")
     private String username;
+    @Email(message = "O campo [email] deve conter um endereço de email válido.")
     private String email;
+    @Length(min = 10, max = 100, message = "O campo [password] deve conter no mínimo 10 caracteres.")
     private String password;
     private String description;
     private String curriculum;
