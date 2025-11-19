34f516ae097afe880f3b043167d75602ced1ff35
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-19 20:50:23 -0300

    "feat: Define os atributos que serão utilizados na criação de um Job."

--- src/main/java/br/com/ruangomes/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/ruangomes/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,12 +1,17 @@
 package br.com.ruangomes.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ruangomes.gestao_vagas.modules.company.dto.CreatJobDTO;
 import br.com.ruangomes.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.ruangomes.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +28,16 @@ public class JobController {
     @PostMapping("/")
     
     
-    public ResponseEntity<Object> create (@Valid @RequestBody JobEntity jobEntity) {
-        try {
-            var result = this.createJobUseCase.execute(jobEntity);
-            return ResponseEntity.ok().body(result);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public JobEntity create (@Valid @RequestBody CreatJobDTO creatJobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id").toString();
+
+        var jobEntity = JobEntity.builder()
+            .description(creatJobDTO.getDescription())
+            .benefits(creatJobDTO.getBenefits())
+            .level(creatJobDTO.getLevel())
+            .companyId(UUID.fromString(companyId))
+            .build();
+        return this.createJobUseCase.execute(jobEntity);
 
     }
 }
