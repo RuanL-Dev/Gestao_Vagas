108c592f2989983709fd5ca0d885a47f1555455a
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-19 22:31:02 -0300

    "feat: Implementa autenticação para Candidate com comparação de senha criptografada e geração de token."

---
+++ src/main/java/br/com/ruangomes/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,63 @@
+package br.com.ruangomes.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.ruangomes.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.ruangomes.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import br.com.ruangomes.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+    
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+            throw new UsernameNotFoundException("Username/password incorrect");
+        });
+
+        var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if(!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+            .withIssuer("javagas")
+            .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withSubject(candidate.getId().toString())
+            .sign(algorithm);
+        
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+            .access_token(token)
+            .build();
+        
+        return authCandidateResponse;
+
+
+    }
+}
+
