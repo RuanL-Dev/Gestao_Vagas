82ca4322506766aaf19132b69fcdabddfc77aae7
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-28 19:19:32 -0300

    "refactor: Altera nome para SecurityCompanyFilter para melhor clareza"

--- src/main/java/br/com/ruangomes/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/ruangomes/gestao_vagas/security/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     @Autowired
-    private SecurityCompanyFilter securityFilter;
+    private SecurityCompanyFilter securityCompanyFilter;
 
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
@@ -30,7 +30,7 @@ public class SecurityConfig {
             .requestMatchers("/candidate/auth").permitAll();
             auth.anyRequest().authenticated();
         }).addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
-        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+        .addFilterBefore(securityCompanyFilter, BasicAuthenticationFilter.class);
         
         return http.build();
     }
