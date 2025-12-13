10ed940a26a5d3beb8ca6bf10c3e0f12e2bbe979
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-13 17:35:35 -0300

    "feat: Implement JUnit tests and Mock dependencies for ApplyJobCandidateUseCase"

---
+++ src/main/java/br/com/ruangomes/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,32 @@
+package br.com.ruangomes.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ruangomes.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.ruangomes.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.ruangomes.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.ruangomes.gestao_vagas.modules.company.repositories.JobRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyJobCandidateUseCase {
+
+    private final CandidateRepository candidateRepository;
+
+    private final JobRepository jobRepository;
+
+    public void execute(UUID idCandidate, UUID idJob) {
+        this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                    throw new UserNotFoundException();
+                });
+
+        this.jobRepository.findById(idJob)
+                .orElseThrow(() -> {
+                    throw new JobNotFoundException();
+                });
+    }
+}
